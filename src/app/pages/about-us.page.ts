8879705ed1ac38108def715ea873486791c9f86c
gradzio <gregradzio@gmail.com>
2022-03-03 18:24:47 +0100

    add home and about-us pages

---
+++ src/app/pages/about-us.page.ts
@@ -0,0 +1,13 @@
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+@Component({
+  selector: 'lib-about-us-page',
+  templateUrl: './about-us.page.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutUsPage {}
